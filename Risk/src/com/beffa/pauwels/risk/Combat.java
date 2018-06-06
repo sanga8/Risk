b27a7b00d897b206507079fcfe2c830cab1daf42
Bargane <Bargane@LAPTOP-0LMLP5DM>
2018-06-06 22:57:55 +0200

    resoudre

--- Risk/src/com/beffa/pauwels/risk/Combat.java
+++ Risk/src/com/beffa/pauwels/risk/Combat.java
@@ -64,14 +64,12 @@ public class Combat {
 		Collections.reverse(liste);
 
 		for (int i = 0; i < liste.size() - 1; i++) {
-			if (liste.get(i).getResultatDe() == liste.get(i + 1).getResultatDe()
-					&& liste.get(i).getPrioriteATT() > liste.get(i + 1).getPrioriteATT()) {
+			if (liste.get(i).getResultatDe() == liste.get(i + 1).getResultatDe() && liste.get(i).getPrioriteATT() > liste.get(i + 1).getPrioriteATT()) {
 				Collections.swap(liste, i, i + 1);
 			}
-			if (liste.size() >= 3) {
+			if (liste.size() == 3) {
 				// EXCEPTION SI LES TROIS ONT LE MEME RESULTAT DE
-				if (liste.get(0).getResultatDe() == liste.get(1).getResultatDe()
-						&& liste.get(1).getResultatDe() == liste.get(2).getResultatDe()) {
+				if (liste.get(0).getResultatDe() == liste.get(1).getResultatDe() && liste.get(1).getResultatDe() == liste.get(2).getResultatDe()) {
 					Collections.sort(liste, Comparator.comparing(Unite::getPrioriteATT));
 				}
 			}
@@ -95,22 +93,26 @@ public class Combat {
 		}
 	}
 
-	public void resoudre(ArrayList<Unite> liste, Territoire tDEF) {
-		for (int i = 0; i < tDEF.listeUnites.size(); i++) {
-			if (liste.get(i).getResultatDe() > tDEF.listeUnites.get(i).getResultatDe()) {
-				// troupe defenseur meurt
-			} else {// troupe attaquant meurt
+	public void resoudre(ArrayList<Unite> listeATT, ArrayList<Unite> listeDEF) {
+		
+		if (listeDEF.size() < listeATT.size()) {
 
-			}
-		}
-		if (liste.size() < tDEF.listeUnites.size()) {
-			for (int i = 0; i < liste.size(); i++) {
-				if (liste.get(i).getResultatDe() > tDEF.listeUnites.get(i).getResultatDe()) {
-					// troupe defenseur meurt
-				} else {// troupe attaquant meurt
-
+			for (int i = 0; i < listeDEF.size(); i++) {
+				if (listeATT.get(i).getResultatDe() > listeDEF.get(i).getResultatDe()) {
+					listeDEF.remove(i);
+				} else {
+					listeATT.remove(i);
 				}
 			}
+		if (listeATT.size() < listeDEF.size()) {
+			for (int i = 0; i < listeATT.size(); i++) {
+				if (listeATT.get(i).getResultatDe() > listeDEF.get(i).getResultatDe()) {
+					listeDEF.remove(i);
+				} else {
+					listeATT.remove(i);
+				}
+			}
+		}
 		}
 	}
 
@@ -122,12 +124,15 @@ public class Combat {
 		 * les territoires réinitialiser les combats
 		 */
 
-		choisirDEF(tDEF.listeUnites);
-		lanceDe(tDEF.listeUnites);
-		trierDEF(tDEF.listeUnites);
-
+		choisirDEF(uniteBatailleDEF);
+		
 		lanceDe(uniteBatailleATT);
+		lanceDe(uniteBatailleDEF);
+		
 		trierATT(uniteBatailleATT);
+		trierDEF(uniteBatailleDEF);
+		
+		resoudre(uniteBatailleATT,uniteBatailleDEF);
 
 	}
 
